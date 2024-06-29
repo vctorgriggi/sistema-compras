08dc886743bf2b50937bdc0b8584500945825a4d
vctorgriggi <victor.gsilva@al.infnet.edu.br>
2024-06-28 20:22:01 -0400

    style: alterar design da tela de login

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,14 +3,14 @@ import Avatar from "@mui/material/Avatar";
 import CssBaseline from "@mui/material/CssBaseline";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate, Link as RouterLink } from "react-router-dom";
-import Link from "@mui/material/Link";
 
 import Copyright from "../components/Copyright";
 import FeedbackSnackbar from "../components/FeedbackSnackbar";
@@ -21,7 +21,7 @@ import Basic from "../components/button/Basic";
 
 const defaultTheme = createTheme();
 
-export default function SignIn() {
+export default function SignInSide() {
   const [isLoadingAnimation, setIsLoadingAnimation] = React.useState(false);
 
   /**
@@ -63,82 +63,100 @@ export default function SignIn() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
-        <Box
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
           sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1641831705160-5d56ac4094cb")',
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "left",
           }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h6">
-            Fazer logon
-          </Typography>
-          <Typography variant="subtitle1">
-            Para sua proteção, verifique a sua identidade.
-          </Typography>
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <BasicTextField
-              required
-              id="email"
-              label="Email"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <BasicTextField
-              required
-              id="password"
-              label="Senha"
-              type="password"
-              name="password"
-              autoComplete="current-password"
-            />
-            {/*            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Permanecer conectado"
-            /> */}
-            <Basic
-              disabled={isLoadingAnimation}
-              type="submit"
-              fullWidth
-              sx={{ mt: 3, mb: 2 }}
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h6">
+              Fazer logon
+            </Typography>
+            <Typography variant="subtitle1">
+              Para sua proteção, verifique a sua identidade.
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
             >
-              {isLoadingAnimation && (
-                <CircularIndeterminate size={25} color="inherit" />
-              )}
-              {!isLoadingAnimation && "Sign In"}
-            </Basic>
-
-            <Grid container>
-              {/* <Grid item xs>
-                <Link component={RouterLink} to="" variant="body2">
-                  Redefina sua senha
-                </Link>
-              </Grid> */}
-              <Grid item>
-                <Link component={RouterLink} to="/sign-up">
-                  Não tem uma conta? Cadastre-se
-                </Link>
+              <BasicTextField
+                required
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <BasicTextField
+                required
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              {/* <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              /> */}
+              <Basic
+                type="submit"
+                fullWidth
+                disable={isLoadingAnimation}
+                sx={{ mt: 3, mb: 2 }}
+              >
+                {isLoadingAnimation && (
+                  <CircularIndeterminate size={25} color="inherit" />
+                )}
+                {!isLoadingAnimation && "Sign In"}
+              </Basic>
+              <Grid container>
+                {/* <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid> */}
+                <Grid item>
+                  <Link component={RouterLink} to="/sign-up" variant="body2">
+                    {"Não tem uma conta? Cadastre-se"}
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
-          </Box>
-        </Box>
 
-        {/**
-         *
-         */}
-        <Copyright sx={{ mt: 8, mb: 4 }} />
+              {/**
+               *
+               */}
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
 
         {/**
          *
@@ -149,7 +167,7 @@ export default function SignIn() {
           message={messageSnackbar}
           onClose={() => setOpenSnackbar(false)}
         />
-      </Container>
+      </Grid>
     </ThemeProvider>
   );
 }
